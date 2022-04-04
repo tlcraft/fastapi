64908f19ff5a8375aaf055b7302d8c4b035dcf3a
Travis Craft <tlcraft@users.noreply.github.com>
2022-04-04 12:44:31 -0600

    Add example endpoint.

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
