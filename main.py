be742a0019a28d25fbf37aef1ab65508e30eefcf
Travis Craft <tlcraft@users.noreply.github.com>
2022-04-06 15:54:25 -0600

    Add enum example.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,3 +13,18 @@ async def root():
 @app.get("/item/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
