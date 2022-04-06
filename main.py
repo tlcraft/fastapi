7385a95688b32145aefb50e616597c5dac084724
Travis Craft <tlcraft@users.noreply.github.com>
2022-04-06 16:14:43 -0600

    Add post example.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,11 +12,23 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+class Item(BaseModel):
+    id: str
+    type: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+@app.post("/item/")
+async def create_item(payload: Item):
+    return payload
+
 @app.get("/item/{item_id}")
-async def read_item(item_id: int, include_name: bool, include_location: Optional[bool] = None):
+async def read_item(item_id: int, include_name: bool, include_create_date: bool = True, include_location: Optional[bool] = None):
     return {
         "item_id": item_id,
         "include_name": include_name,
+        "include_create_date": include_create_date,
         "include_location": include_location    
     }
 
