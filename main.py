a1992b42c86ec1e0167604be24a8b2e6fdd21003
Travis Craft <tlcraft@users.noreply.github.com>
2022-05-03 18:23:49 -0600

    Update auth.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
 from enum import Enum
 from typing import Optional
+from database_mock import fake_users_db
 from fastapi import Body, Depends, FastAPI, Header, HTTPException, Path, Query, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field
 from user import User
 
@@ -23,25 +24,63 @@ async def yield_dependency_example():
         print(f"Completed the yield in {end - start:0.4f} seconds")
 
 
-app = FastAPI(dependencies=[Depends(verify_test_header), Depends(yield_dependency_example)])
+app = FastAPI(dependencies=[Depends(yield_dependency_example)])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def fake_decode_token(token: str) -> User:
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
+class UserInDB(User):
+    hashed_password: str
 
+def get_db_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_db_user(fake_users_db, token)
+    return user
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+    
 # path operations are evaluated in the order they are defined
 # consider your routes and path parameters
 
 @app.get("/", tags=["Root"])
-async def root(current_user: User = Depends(get_current_user)):
-    return {"message": "Hello World", "user": current_user}
+async def root():
+    return {"message": "Hello World"}
 
 class Item(BaseModel):
     id: str = Field(None, example="3")
