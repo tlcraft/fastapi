a195ab3b80cf9052de6604e2c35f8401e0976880
Travis Craft <tlcraft@users.noreply.github.com>
2022-06-06 17:21:43 -0600

    Add common methods.

--- src/dependencies/dependencies.py
+++ src/dependencies/dependencies.py
@@ -1,5 +1,16 @@
-from fastapi import Depends
+from fastapi import BackgroundTasks, Depends
 from ..models.common_query_params import CommonQueryParams
+from typing import Union
 
 async def common_parameters(commons: CommonQueryParams = Depends(CommonQueryParams), include_all: bool = False):
     return {**commons.__dict__, "include_all": include_all}
+
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+def get_query(background_tasks: BackgroundTasks, q: Union[str, None] = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
