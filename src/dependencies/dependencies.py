16dd2945c940a1f5fb4092111c80fa278e4bb78f
Travis Craft <tlcraft@users.noreply.github.com>
2022-06-11 10:48:21 -0600

    Add methods to dependencies.

--- src/dependencies/dependencies.py
+++ src/dependencies/dependencies.py
@@ -1,4 +1,5 @@
-from fastapi import BackgroundTasks, Depends
+import time
+from fastapi import BackgroundTasks, Depends, Header, HTTPException
 from ..models.common_query_params import CommonQueryParams
 from typing import Union
 
@@ -14,3 +15,17 @@ def get_query(background_tasks: BackgroundTasks, q: Union[str, None] = None):
         message = f"found query: {q}\n"
         background_tasks.add_task(write_log, message)
     return q
+
+async def verify_test_header(x_test_header: str = Header(...)):
+    if x_test_header != "X-Test-Header":
+        raise HTTPException(status_code=400, detail="X-Test-Header value invalid")
+
+async def yield_dependency_example():
+    try:
+        start = time.perf_counter()
+        print("Start: ", start)
+        yield start
+    finally:
+        end = time.perf_counter()
+        print("End: ", end)
+        print(f"Completed the yield in {end - start:0.4f} seconds")
