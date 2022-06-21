e11d2706ac18f5bcb1e91aa40e7145a5c5cdd038
Travis Craft <tlcraft@users.noreply.github.com>
2022-06-21 16:38:15 -0600

    Add dependency examples.

--- src/dependencies/dependencies.py
+++ src/dependencies/dependencies.py
@@ -29,3 +29,12 @@ async def yield_dependency_example():
         end = time.perf_counter()
         print("End: ", end)
         print(f"Completed the yield in {end - start:0.4f} seconds")
+
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
