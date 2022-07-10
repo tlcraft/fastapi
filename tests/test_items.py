47aba404466e190027f0cd9fd1b150c31b3e73a4
Travis Craft <tlcraft@users.noreply.github.com>
2022-07-10 15:26:52 -0600

    Add update item test.

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,3 +1,4 @@
+import json
 from fastapi.testclient import TestClient
 from src.main import app
 
@@ -14,4 +15,22 @@ def test_read_item_by_id():
         "include_create_date": True,
         "include_location": None,
         "query": None  
+    }
+
+def test_update_item_by_id():
+    item = { 
+        "id": 1,
+        "type": "type", 
+        "description": "description", 
+        "price": 12.2, 
+        "tax": 1.1 
+    }
+    headers = { "X-Token": "test-token" }
+
+    response = client.put("/items/1", data=json.dumps(item), headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "1",
+        "header": "test-token"
     }
